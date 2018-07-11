da56c0ae989bcff80a5681010a158b8dff78bb40
gushuai <gushuai@meituan.com>
2018-07-11 14:48:20 +0800

    add new connents

--- test_git/First.java
+++ test_git/First.java
@@ -2,15 +2,30 @@
 * @Author: Marte
 * @Date:   2018-07-11 10:51:45
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-07-11 11:16:28
+* @Last Modified time: 2018-07-11 14:47:23
 */
 
-public class java {
+public class First {
 
     ArrayList<Integer> list = new ArrayList<Integer>();
 
+
+    public void setlist(){
+        list.add("string");
+        list.add("sql");
+        list.add("git commands");
+    }
+
+
+   public void printList(){
+      System.out.println(list);
+   }
+
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        First first = new First();
+        first.setlist();
+        first.printList();
     }
+
 }
