44633e71bc4c89d7a276186197916459b5ff41d8
gushuai <gushuai@meituan.com>
2018-07-11 11:26:29 +0800

    First commit

---
+++ test_git/First.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Marte
+* @Date:   2018-07-11 10:51:45
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-07-11 11:16:28
+*/
+
+public class java {
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
