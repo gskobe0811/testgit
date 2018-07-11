0d1fd33e6f3aa17b0832b836e8c7eedb446dfd5b
gushuai <gushuai@meituan.com>
2018-07-11 16:17:54 +0800

    新增文件

---
+++ test_git/Second.java
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2018-07-11 16:15:55
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-07-11 16:16:46
+*/
+
+public class java {
+    public static void main(String[] args) {
+
+      System.out.println("study git commands");
+
+    }
+}
